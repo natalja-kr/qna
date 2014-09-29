ecb7a177e75ec3f545742c6a306e5dd269da9efa
Natalja <natalja_kr@tut.by>
2014-09-29 20:51:05 +0300

    add User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-
+  validates :name, presence: true, uniqueness: true
+  validates_length_of :name, minimum: 3, maximum: 30
+  has_many :questions
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it { should validate_presence_of :name}
+  it { should validate_uniqueness_of :name}
+  it { should have_many :questions}
+  it { should ensure_length_of(:name).is_at_least(3) }
+  it { should ensure_length_of(:name).is_at_most(30) }
 end
