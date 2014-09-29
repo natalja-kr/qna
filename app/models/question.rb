85a6a7e61f477cbcb3e4572786e706d5781367cc
Natalja <natalja_kr@tut.by>
2014-09-29 20:45:24 +0300

    add Question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   validates :title, :body, presence: true
+  validates_length_of :title, minimum: 15, maximum: 200
+  belongs_to :user
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, :type => :model do
   it { should validate_presence_of :title}
   it { should validate_presence_of :body}
+  it { should belong_to :user}
+  it { should ensure_length_of(:title).is_at_least(15) }
+  it { should ensure_length_of(:title).is_at_most(200) }
 end
