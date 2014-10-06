1474c2ca16cacb1857bffc6b0499ccbeee362439
Natalja <natalja_kr@tut.by>
2014-10-06 21:59:22 +0300

    add answer controller

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -3,6 +3,13 @@
 FactoryGirl.define do
   factory :answer do
     body "MyText"
-    user nil
+    user ""
+    question ""
+  end
+
+  factory :invalid_answer, class: "Answer" do
+    body nil
+    user ""
+    question ""
   end
 end
