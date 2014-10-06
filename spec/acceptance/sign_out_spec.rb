2a915f9b55d66d4d7fed939327627a8455242df8
Natalja <natalja_kr@tut.by>
2014-10-06 11:31:51 +0300

    sign_out spec

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  In order to be able to maintain confidentiality
+  As an user
+  I want to be able to sign out
+} do
+  given(:user) {create(:user)}
+  scenario 'Registered user try to sign out' do
+    sign_in(user)
+
+    page.driver.submit :delete, "/users/sign_out", {}
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+end
