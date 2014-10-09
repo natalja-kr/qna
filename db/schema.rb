2cbb6088a7b7a37e55380a2077e929909dd699e6
Natalja <natalja_kr@tut.by>
2014-10-09 22:28:34 +0300

    Attachment model and association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141009191925) do
+ActiveRecord::Schema.define(version: 20141009192522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,10 @@ ActiveRecord::Schema.define(version: 20141009191925) do
     t.string   "file"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "question_id"
   end
+
+  add_index "attachments", ["question_id"], name: "index_attachments_on_question_id", using: :btree
 
   create_table "questions", force: true do |t|
     t.string   "title"
